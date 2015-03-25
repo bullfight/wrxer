5fb61d2169cd567e7b13fe4941e76613f6c8e9e2
Patrick Schmitz <pat@reccenter.com>
2015-03-24 20:59:37 -0700

    add wordpress fixture

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'wrxer'
 require 'pry'
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
+
